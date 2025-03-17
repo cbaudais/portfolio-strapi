6919511b8c9d6e81a77624683a12e0a68e5149ca
cbaudais <christinabaudais@cmail.carleton.ca>
2025-03-17 15:29:17 -0600

    cat pages loading title & metadata

--- frontend/app/(pages)/[category]/page.tsx
+++ frontend/app/(pages)/[category]/page.tsx
@@ -1,3 +1,4 @@
+
 import { FallbackFetch } from "@/components/ErrorFallback";
 import { Section } from "@/components/Section";
 import PostList from "@/components/PostList";
@@ -5,12 +6,12 @@ import { ErrorBoundary } from "react-error-boundary";
 import { fetchAPI } from "@/utils/fetch-api";
 import { strapiCategories } from "@/types/types";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
 //fetch posts, filtered by categories
 async function fetchByCategory(filter: string) {
     try {
         const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-        const options = { headers: { Authorization: `Bearer ${token}` } };
         const query = '/projects';
         const urlParamsObject = {
             filters: {
@@ -27,6 +28,7 @@ async function fetchByCategory(filter: string) {
                 }
             },
         };
+        const options = { headers: { Authorization: `Bearer ${token}` } };
         const responseData = await fetchAPI(query, urlParamsObject, options);
         return responseData;
     } catch (error) {
@@ -34,10 +36,31 @@ async function fetchByCategory(filter: string) {
     }
 }
 
+export async function generateMetadata({ params }: { params: Promise<{ category: string }> }): Promise<Metadata> {
+    const filter = (await params).category;
+    const { data } = await fetchByCategory(filter);
+    let exitLoop = false;
+    let catName = filter;
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].categories.length; j++) {
+            if (data[i].categories[j].slug.includes(filter)) {
+                catName = data[i].categories[j].name;
+                exitLoop = true;
+                break;
+            }
+        }
+        if (exitLoop) { break; }
+    }
+    // if (data.length === 0) { return notFound() }
+    return {
+        title: catName + " | Christina Baudais",
+        description: "Christina Baudais's " + catName
+    }
+}
+
 export default async function CategoryRoute({ params }: { params: Promise<{ category: string }> }) {
     const filter = (await params).category;
     const { data } = await fetchByCategory(filter);
-
     let exitLoop = false;
     let catName = filter;
     for (let i = 0; i < data.length; i++) {
@@ -65,13 +88,12 @@ export default async function CategoryRoute({ params }: { params: Promise<{ cate
 }
 
 // Return a list of `params` to populate the [slug] dynamic segment
-export async function generateStaticParams({ params }: { params: Promise<{ category: string }> }) {
-    const filter = (await params).category;
-    const { data } = await fetchByCategory(filter);
-    if (data.length === 0) { return notFound() };
-    console.log(data);
+// export async function generateStaticParams({ params }: { params: Promise<{ category: string }> }) {
+//     const filter = (await params).category;
+//     const { data } = await fetchByCategory(filter);
+//     if (data.length === 0) { return notFound() };
 
-    return data.map((category: strapiCategories) => ({
-        slug: category.slug,
-    }))
-}
+//     return data.map((category: strapiCategories) => ({
+//         slug: category.slug,
+//     }))
+// }
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,23 +4,19 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import { ErrorBoundary } from "react-error-boundary";
 import { FallbackFetch } from "./components/ErrorFallback";
+import { fetchAPI } from "./utils/fetch-api";
 
-export const metadata: Metadata = {
-  title: "Christina Baudais | Portfolio",
-  description: "Christina Baudais's Multimedia Portfolio",
-};
 
-export default function RootLayout({
+// export const metadata: Metadata = {
+//   title: "Christina Baudais | Portfolio",
+//   description: "Christina Baudais's Multimedia Portfolio",
+// };
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-  // const options = { headers: { Authorization: `Bearer ${token}` } };
-  // const query = '/categories';
-  // const urlParamsObject = {
-  //   sort: { name: "asc" }
-  // }
   return (
     <html lang="en">
       <body>
